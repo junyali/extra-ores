449f6ec941919795c5117a1022b7e76545a95ad7
Junya <junjia.liu@pm.me>
2025-05-02 23:58:22 +0100

    Update Datagen (WIP still T-T)

--- src/client/java/io/github/junyali/extra_ores/datagen/EXtraOresDataGenerator.java
+++ src/client/java/io/github/junyali/extra_ores/datagen/EXtraOresDataGenerator.java
@@ -4,8 +4,13 @@ import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.minecraft.data.recipe.RecipeExporter;
+import net.minecraft.data.recipe.RecipeGenerator;
 import net.minecraft.item.Item;
+import net.minecraft.recipe.book.RecipeCategory;
+import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.registry.tag.TagKey;
@@ -13,6 +18,7 @@ import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 import static io.github.junyali.extra_ores.EXtraOres.MOD_ID;
@@ -29,7 +35,10 @@ public class EXtraOresDataGenerator implements DataGeneratorEntrypoint {
 
         pack.addProvider(OreTagGenerator::new);
         pack.addProvider(EnglishLangProvider::new);
+        pack.addProvider(RecipeProvider::new);
     }
+
+    // TAGS
 
     private static class OreTagGenerator extends FabricTagProvider.ItemTagProvider {
         public OreTagGenerator(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> completableFuture) {
@@ -48,6 +57,8 @@ public class EXtraOresDataGenerator implements DataGeneratorEntrypoint {
                     .add(Identifier.of(MOD_ID, "cryo_shard"));
         }
     }
+
+    // LANGUAGES
 
     public static class EnglishLangProvider extends FabricLanguageProvider {
         protected EnglishLangProvider(FabricDataOutput dataOutput, CompletableFuture<RegistryWrapper.WrapperLookup> registryLookup) {
@@ -166,5 +177,92 @@ public class EXtraOresDataGenerator implements DataGeneratorEntrypoint {
             translationBuilder.add("item.extra-ores.void_berries", "Void Berries");
         }
     }
+
+    // RECIPES
+    public static class RecipeProvider extends FabricRecipeProvider {
+        public RecipeProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+            super(output, registriesFuture);
+        }
+
+        @Override
+        protected RecipeGenerator getRecipeGenerator(RegistryWrapper.WrapperLookup registryLookup, RecipeExporter exporter) {
+            return new RecipeGenerator(registryLookup, exporter) {
+                @Override
+                public void generate() {
+                    // Item Registries
+                    Item luminitite = getItem("luminitite");
+                    Item raw_luminitite = getItem("raw_luminitite");
+                    Item luminitite_block = getItem("luminitite_block");
+                    Item infernium_clump = getItem("infernium_clump");
+                    Item infernium_block = getItem("infernium_block");
+                    Item voidium_dust = getItem("voidium_dust");
+                    Item voidium_block = getItem("voidium_block");
+
+                    // Smelting Recipes
+                    offerSmelting(
+                            List.of(raw_luminitite),
+                            RecipeCategory.MISC,
+                            luminitite,
+                            1.0f,
+                            300,
+                            "extra_ores_smelting"
+                    );
+
+                    // Compactable Recipes (Item <-> Block) :3
+                    createShaped(RecipeCategory.BUILDING_BLOCKS, luminitite_block, 1)
+                            .pattern("###")
+                            .pattern("###")
+                            .pattern("###")
+                            .input('#', luminitite)
+                            .criterion(hasItem(luminitite), conditionsFromItem(luminitite))
+                            .offerTo(exporter);
+                    createShapeless(RecipeCategory.MISC, luminitite, 9)
+                            .input(luminitite_block, 1)
+                            .criterion(hasItem(luminitite_block), conditionsFromItem(luminitite_block))
+                            .offerTo(exporter);
+
+                    createShaped(RecipeCategory.BUILDING_BLOCKS, infernium_block, 1)
+                            .pattern("###")
+                            .pattern("###")
+                            .pattern("###")
+                            .input('#', infernium_clump)
+                            .criterion(hasItem(infernium_clump), conditionsFromItem(infernium_clump))
+                            .offerTo(exporter);
+                    createShapeless(RecipeCategory.MISC, infernium_clump, 9)
+                            .input(infernium_block, 1)
+                            .criterion(hasItem(infernium_block), conditionsFromItem(infernium_block))
+                            .offerTo(exporter);
+
+                    createShaped(RecipeCategory.BUILDING_BLOCKS, voidium_block, 1)
+                            .pattern("###")
+                            .pattern("###")
+                            .pattern("###")
+                            .input('#', voidium_dust)
+                            .criterion(hasItem(voidium_dust), conditionsFromItem(voidium_dust))
+                            .offerTo(exporter);
+                    createShapeless(RecipeCategory.MISC, voidium_dust, 9)
+                            .input(voidium_block, 1)
+                            .criterion(hasItem(voidium_block), conditionsFromItem(voidium_block))
+                            .offerTo(exporter);
+
+
+
+
+                }
+
+                private Item getItem(String id) {
+                    return registries.getOrThrow(RegistryKeys.ITEM)
+                            .getOrThrow(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(MOD_ID, id)))
+                            .value();
+                }
+            };
+        }
+
+        @Override
+        public String getName() {
+            return "RecipeProvider";
+        }
+    }
+
 
 }
